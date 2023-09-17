fa2685ecd4088611da11cc4b71e30b03011d2366
Abdul Arif <abdul.arif0705@gmail.com>
2023-09-16 22:25:36 -0400

    get all flagged emails as well

--- generate_and_validate_emails.py
+++ generate_and_validate_emails.py
@@ -2,28 +2,33 @@ import requests
 import time
 import json
 
+
 # Function to send a GET request to AbstractAPI for email validation
 def send_email_validation_request(email_address, api_key):
     try:
         response = requests.get(
-            f"https://emailvalidation.abstractapi.com/v1/?api_key={api_key}&email={email_address}"  # Changed from email_address to email
+            f"https://emailvalidation.abstractapi.com/v1/?api_key={api_key}&email={email_address}"
         )
         return response.json()
     except requests.exceptions.RequestException as api_error:
         print(f"There was an error contacting the Email Validation API: {api_error}")
         return None
 
+
 # Function to determine if the email is valid based on AbstractAPI response
-def is_valid_email(api_data):
+def categorize_email(api_data):
     if api_data is None:
-        return False
+        return "invalid"
 
-    keys = ['is_valid_format', 'is_free_email', 'is_disposable_email',
-            'is_role_email', 'is_catchall_email', 'is_mx_found', 'is_smtp_valid']
+    # Checking if all the keys we are interested in are in the api_data
+    general_keys = ['deliverability', 'quality_score', 'is_valid_format', 'is_free_email', 'is_disposable_email',
+                    'is_role_email', 'is_catchall_email', 'is_mx_found', 'is_smtp_valid']
 
-    if not all(key in api_data for key in keys):
-        return False
+    if not all(key in api_data for key in general_keys):
+        return "invalid"
 
+    deliverability = api_data['deliverability']
+    quality_score = api_data['quality_score']
     is_valid_format = api_data['is_valid_format']['value']
     is_free_email = api_data['is_free_email']['value']
     is_disposable_email = api_data['is_disposable_email']['value']
@@ -32,10 +37,13 @@ def is_valid_email(api_data):
     is_mx_found = api_data['is_mx_found']['value']
     is_smtp_valid = api_data['is_smtp_valid']['value']
 
-    if is_valid_format and is_mx_found and is_smtp_valid:
+    if (deliverability == "DELIVERABLE" and quality_score >= 0.95) and (
+            is_valid_format and is_mx_found and is_smtp_valid):
         if not (is_free_email or is_disposable_email or is_role_email or is_catchall_email):
-            return True
-    return False
+            return "valid"
+        else:
+            return "flagged"
+    return "invalid"
 
 # Generate individual email based on a pattern
 def generate_email(first, last, domain, pattern):
@@ -71,13 +79,31 @@ if __name__ == "__main__":
 
     email_variations = generate_email_variations(first_name, last_name, company_domain)
 
+    valid_emails = []
+    flagged_emails = []
+
     print("\nChecking email address variations for validity...")
     for email in email_variations:
         print(f"Checking {email}...")
         api_response = send_email_validation_request(email, api_key)
         print(f"API response: {api_response}")
-        if is_valid_email(api_response):
+
+        categorization = categorize_email(api_response)
+
+        if categorization == "valid":
             print(f"{email} appears to be valid.")
+            valid_emails.append(email)
+        elif categorization == "flagged":
+            print(f"{email} is flagged for review.")
+            flagged_emails.append(email)
         else:
             print(f"{email} appears to be invalid.")
         time.sleep(1)  # To avoid exceeding rate limits
+
+    print("\nValid Emails:")
+    for email in valid_emails:
+        print(email)
+
+    print("\nFlagged Emails:")
+    for email in flagged_emails:
+        print(email)
